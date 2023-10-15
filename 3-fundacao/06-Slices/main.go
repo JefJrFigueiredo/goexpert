bee70cc68ddb4f43389f5acfe576b8fd3ef44b9b
Jefferson Figueiredo Jr <jefjr2504@gmail.com>
2023-10-15 20:38:50 -0300

    chore: refactoring outputs for better readability

--- 3-fundacao/06-Slices/main.go
+++ 3-fundacao/06-Slices/main.go
@@ -4,14 +4,16 @@ import "fmt"
 
 func main() {
 	s := []int{10, 20, 30, 50, 60, 70, 80, 90, 100}
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+	fmt.Printf("s[:]	len=%d cap=%d %v\n", len(s), cap(s), s)
 
-	fmt.Printf("len=%d cap=%d %v\n", len(s[:0]), cap(s[:0]), s[:0])
+	fmt.Printf("s[:0]	len=%d cap=%d %v\n", len(s[:0]), cap(s[:0]), s[:0])
 
-	fmt.Printf("len=%d cap=%d %v\n", len(s[:4]), cap(s[:4]), s[:4])
+	fmt.Printf("s[:4]	len=%d cap=%d %v\n", len(s[:4]), cap(s[:4]), s[:4])
 
-	fmt.Printf("len=%d cap=%d %v\n", len(s[2:]), cap(s[2:]), s[2:])
+	fmt.Printf("s[2:]	len=%d cap=%d %v\n", len(s[2:]), cap(s[2:]), s[2:])
 
 	s = append(s, 110)
-	fmt.Printf("len=%d cap=%d %v\n", len(s[:2]), cap(s[:2]), s[:2])
+	fmt.Printf("s[:]	len=%d cap=%d %v\n", len(s[:]), cap(s[:]), s[:])
+
+	fmt.Printf("s[:2]	len=%d cap=%d %v\n", len(s[:2]), cap(s[:2]), s[:2])
 }
